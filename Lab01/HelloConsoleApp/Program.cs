6a0a09c0a826f8b08a6e20d7a50cacb898605e43
Michał <michalp10111@gmail.com>
2019-03-17 19:29:41 +0100

    Nowy

---
+++ Lab01/HelloConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imię: ");
+
+            string imie 
+             imie   = ConsoleReadLine();
+
+
+            
+
+
+        }
+    }
+}
