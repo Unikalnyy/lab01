43e05811f2a4c6605b361db59641e91ff69f2b4c
Michał <michalp10111@gmail.com>
2019-03-19 20:35:48 +0100

    Postęp 1.0

--- Lab01/HelloConsoleApp/Program.cs
+++ Lab01/HelloConsoleApp/Program.cs
@@ -10,15 +10,28 @@ namespace HelloConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj imię: ");
+            //    Console.WriteLine("Podaj imię: ");
+            //    string imie = Console.ReadLine();
 
-            string imie 
-             imie   = ConsoleReadLine();
+            //    Console.WriteLine("Podaj nazwisko: ");
+            //    string nazwisko = Console.ReadLine();
 
+            //    Console.WriteLine("Witaj, " + imie + " " + nazwisko);
 
-            
+            //    Console.WriteLine("Witaj {0} {1}. Czy Pan {0} w rzeczywistości nazywa się {1}" ,imie,nazwisko);
 
+            Console.WriteLine("System do sprawdzania ile lat zostało do Emerytury.");
+            Console.WriteLine("Podaj swój wiek: ");
+            int wiek = Convert.ToInt32(Console.ReadLine());
 
+            if(wiek < 67)
+            {
+                Console.WriteLine("Lata pozostałe do emerytury: {0}", 67 - wiek);
+            }
+            else
+            {
+                Console.WriteLine("Gratulacje! Możesz cieszyć się emeryturą!");
+            }
         }
     }
 }
