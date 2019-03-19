5b0719a5bd84ec0830e9831b8c2d6a09d8d6aafc
Michał <michalp10111@gmail.com>
2019-03-19 21:29:23 +0100

    End Lab01

--- Lab01/HelloConsoleApp/Program.cs
+++ Lab01/HelloConsoleApp/Program.cs
@@ -10,21 +10,23 @@ namespace HelloConsoleApp
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Podaj imię: ");
-            //    string imie = Console.ReadLine();
+            Console.WriteLine("Podaj imię: ");
+            string imie = Console.ReadLine();
 
-            //    Console.WriteLine("Podaj nazwisko: ");
-            //    string nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} w rzeczywistości nazywa się {1}" ,imie,nazwisko);
 
-            //    Console.WriteLine("Witaj, " + imie + " " + nazwisko);
-
-            //    Console.WriteLine("Witaj {0} {1}. Czy Pan {0} w rzeczywistości nazywa się {1}" ,imie,nazwisko);
+            Console.WriteLine("");
 
             Console.WriteLine("System do sprawdzania ile lat zostało do Emerytury.");
             Console.WriteLine("Podaj swój wiek: ");
             int wiek = Convert.ToInt32(Console.ReadLine());
-
-            if(wiek < 67)
+            if(wiek < 0)
+            {
+                Console.WriteLine("Błedne dane!");
+            }
+            else if(wiek < 67)
             {
                 Console.WriteLine("Lata pozostałe do emerytury: {0}", 67 - wiek);
             }
