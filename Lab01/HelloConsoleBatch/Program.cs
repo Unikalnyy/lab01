63d0418e856a898ae570c8d4db92dd068644723e
Michał <michalp10111@gmail.com>
2019-03-20 18:24:43 +0100

    Praca Domowa 1

--- Lab01/HelloConsoleBatch/Program.cs
+++ Lab01/HelloConsoleBatch/Program.cs
@@ -10,28 +10,51 @@ namespace HelloConsoleBatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program na powitanie.");
-            string imie = args[0];
-            string nazwisko = args[1];
-            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} w rzeczywistości nazywa się {1}", imie, nazwisko);
 
 
-            Console.WriteLine("System do sprawdzania ile lat zostało do Emerytury.");
-            int wiek = Convert.ToInt32(args[2]);
-            if (wiek < 0)
+            Console.WriteLine("Program na powitanie! Wykonał: Michał P.");
+            string imie;
+            string nazwisko;
+            int wiek;
+
+            if (args.Length == 3)
             {
-                Console.WriteLine("Błedne dane!");
+                imie = args[0];
+                nazwisko = args[1];
+                wiek = Convert.ToInt32(args[2]);
+
+                Console.WriteLine("Witaj {0} {1}", imie, nazwisko);
+                Console.WriteLine("System do sprawdzania ile lat zostało do Emerytury.");
+                if (wiek < 0)
+                {
+                    Console.WriteLine("Błedne dane!");
+                    Console.ReadKey();
+                }
+                else if (wiek < 67)
+                {
+                    Console.WriteLine("Lata pozostałe do emerytury: {0}", 67 - wiek);
+                    Console.ReadKey();
+                }
+                else
+                {
+                Console.WriteLine("Gratulacje! Możesz cieszyć się emeryturą!");
                 Console.ReadKey();
+                }
             }
-            else if (wiek < 67)
+            else if (args.Length == 2)
             {
-                Console.WriteLine("Lata pozostałe do emerytury: {0}", 67 - wiek);
-                Console.ReadKey();
+                imie = args[0];
+                nazwisko = args[1];
+                Console.WriteLine("Witaj {0} {1}", imie, nazwisko);
+            }
+            else if (args.Length == 1)
+            {
+                imie = args[0];
+                Console.WriteLine("Witaj, " + imie);
             }
             else
             {
-                Console.WriteLine("Gratulacje! Możesz cieszyć się emeryturą!");
-                Console.ReadKey();
+                Console.WriteLine("Brak Danych!");
             }
         }
     }
